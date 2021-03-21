449f12586fffb5190fbefce21f431aca52cb2a0c
wallacemachado <wallacefm18@gmail.com>
2021-03-21 15:44:09 -0300

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'; //useState para usar o Hooks permite que as functions tenham state
+import React, { useState, useEffect } from 'react'; //useState para usar o Hooks permite que as functions tenham state
 
 
 function App() {
@@ -11,6 +11,22 @@ function App() {
 
     // Declarar uma nova variável de state, na qual chamaremos de "contador"
     const [contador, setContador] = useState(0);
+
+    //equivale ao component DidMount, é o primeiro a ser executado quando o propgrama é carregado
+    useEffect(()=>{
+      const tarefasStorage = localStorage.getItem('tarefas');
+      // passa para a state tarefas o que está no localStorage
+      if(tarefasStorage){
+        setTarefas(JSON.parse(tarefasStorage));
+      }
+  
+    }, []);
+  
+  //equivale é o Component DidUpdate, é executado quando ten alteração no array [tarefas]
+    useEffect(()=> {
+      //salva as alterações feitas no Array [tarefas] no localStorage
+      localStorage.setItem('tarefas', JSON.stringify(tarefas));
+    }, [tarefas]);
 
   function handleAdd(){
     setTarefas([...tarefas, input])
