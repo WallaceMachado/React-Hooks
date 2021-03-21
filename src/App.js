6bdf69e887ff24062aab8793d498d5967323c710
wallacemachado <wallacefm18@gmail.com>
2021-03-21 15:25:30 -0300

    contador

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
   ]);
   const [input, setInput] = useState('');
 
+    // Declarar uma nova variável de state, na qual chamaremos de "contador"
+    const [contador, setContador] = useState(0);
+
   function handleAdd(){
     setTarefas([...tarefas, input])
     setInput('');
@@ -27,6 +30,11 @@ function App() {
 
       <input type="text" value={input} onChange={e => setInput(e.target.value)}/>    
       <button type="button" onClick={handleAdd}>Adicionar</button>
+
+      <p>You clicked {contador} times</p>
+      <button onClick={() => setContador(contador + 1)}>
+          Aumentar
+      </button>
 
     </div>
   );
